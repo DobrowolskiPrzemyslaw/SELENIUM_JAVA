17ff76752830e6b6f4af16898c79363dbe8784ec
Przemyslaw Dobrowolski <dobrowolski.przemyslaw.1991@gmail.com>
2023-01-19 20:28:42 +0100

    City name test

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -1,5 +1,7 @@
 
+import elements.Button;
 import elements.InputTextBox;
+import elements.Label;
 import elements.Link;
 import org.openqa.selenium.By;
 import org.testng.annotations.*;
@@ -9,10 +11,20 @@ public class HotelSearch extends BaseTest {
     @Test
     public void searchHotel(){
         driver.get("http://www.kurs-selenium.pl/demo/");
-        InputTextBox searchInputHotel = new InputTextBox(By.xpath("//span[text()='Search by Hotel or City Name']"));
+        Label labelOnInput = new Label(By.xpath("//span[text()='Search by Hotel or City Name']"));
+        InputTextBox searchInputHotel = new InputTextBox(By.xpath("//div[@id='select2-drop']//input"));
+        InputTextBox checkIn = new InputTextBox(By.xpath("//input[@class='form input-lg dpd1']"));
+        InputTextBox checkOut = new InputTextBox(By.xpath("//input[@class='form input-lg dpd2']"));
+        InputTextBox checkPeople = new InputTextBox(By.xpath("//input[@class='form form-control input-lg collapsed']"));
         Link lintToHotels = new Link(By.xpath("//div[text()=', United Arab Emirates']"));
+        Button searchButton = new Button(By.xpath("//button[text()=' Search']"));
 
+        labelOnInput.clickOnElement();
         searchInputHotel.sendText("Dubai");
-//        lintToHotels.clickOnLink();
+        lintToHotels.clickOnLink();
+        searchButton.clickOnElement();
+        checkIn.sendText("19/01/2023");
+        checkOut.sendText("19/01/2023");
+        checkPeople.c
     }
 }
