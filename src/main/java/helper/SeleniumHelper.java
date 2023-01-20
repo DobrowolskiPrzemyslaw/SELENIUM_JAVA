c1e2129ee447cba3e4a061de42f9430bab85378a
Przemyslaw Dobrowolski <dobrowolski.przemyslaw.1991@gmail.com>
2023-01-20 19:15:21 +0100

    change method takeScreenShot

--- src/main/java/helper/SeleniumHelper.java
+++ src/main/java/helper/SeleniumHelper.java
@@ -6,23 +6,21 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SeleniumHelper {
 
-    public static String takeScreenShot(WebDriver driver) throws IOException {
+    public static void takeScreenShot() throws IOException {
+        WebDriver driver = DriverManager.getDriver();
         TakesScreenshot takeScreenShot = (TakesScreenshot) driver;
         File screenshotFile = takeScreenShot.getScreenshotAs(OutputType.FILE);
-        File destinationFile = new File("src//test//resources//screens//name " + LocalTime.now().getNano() + ".png");
+        File destinationFile = new File( "src//test//java//resources//" + LocalTime.now().getNano() + ".png");
         Files.copy(screenshotFile.toPath(), destinationFile.toPath());
-        return destinationFile.getAbsolutePath();
     }
 
     public static List<String> sortOfList(List<WebElement> list) {
-//        return list.stream().map(el -> el.getText()).sorted().collect(Collectors.toList());
         return list.stream().map(el -> el.getAttribute("textContent")).sorted().collect(Collectors.toList());
     }
 
