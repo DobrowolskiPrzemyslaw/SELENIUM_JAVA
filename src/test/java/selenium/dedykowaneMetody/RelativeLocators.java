8478cf7007fb910cb1658ca47c3d3a3cde59a23f
Przemyslaw Dobrowolski <dobrowolski.przemyslaw.1991@gmail.com>
2023-03-08 11:43:10 +0100

    added relatives locators

---
+++ src/test/java/selenium/dedykowaneMetody/RelativeLocators.java
@@ -0,0 +1,63 @@
+package selenium.dedykowaneMetody;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.Test;
+import selenium.DriverManager;
+import selenium.baseTests.BaseTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class RelativeLocators extends BaseTest {
+    WebDriver driver;
+
+    @Test
+    public void above(){
+        driver = DriverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(RelativeLocator.with(By.tagName("input")).above(By.id("password")));
+    }
+
+    @Test
+    public void below(){
+        driver = DriverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(RelativeLocator.with(By.tagName("input")).above(By.id("password")));
+    }
+
+    @Test
+    public void leftOf(){
+        driver = DriverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(RelativeLocator.with(By.tagName("button")).toLeftOf(By.id("submit"));
+    }
+
+
+    @Test
+    public void rightOf(){
+        driver = DriverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(RelativeLocator.with(By.tagName("button")).toRightOf(By.id("cancel")));
+    }
+
+    @Test
+    public void near(){
+        driver = DriverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(RelativeLocator.with(By.tagName("input")).near(By.id("lbl-email")));
+    }
+
+    @Test
+    public void chainingLocators(){
+        driver = DriverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(RelativeLocator.with(By.tagName("button")).below(By.id("email")).toRightOf(By.id("cancel")));
+    }
+}
